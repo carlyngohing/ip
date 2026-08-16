554d3f9994d81c7729cc7854f889e3dedc874957
carlyngohing <e1522842@u.nus.edu>
2026-08-16 21:55:07 +0800

    Add greetings

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,5 +7,9 @@ public class Duke {
     "   ██║   ██║  ██║██║     ██║  ██║   ██║   ██║  ██║╚██████╔╝██║ ╚████║\n" +
     "   ╚═╝   ╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝";
         System.out.println(banner);
+        System.out.println("Hello Hello!  I'm Yapatron :D");
+        System.out.println("What can I do for you?");
+        System.out.println();
+        System.out.println("Bye!! Hope to see you again soon :)");
     }
 }
